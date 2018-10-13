ba5f49f1116da29f62d8481dfa3f998fa916b702
lexville <alekikangethe@gmail.com>
2018-10-14 02:12:38 +0300

    Add the post and get user handlers

--- controllers/repocontroller.go
+++ controllers/repocontroller.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"GitMe/view"
+	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Repocontroller contains all the fields needed by
@@ -27,4 +30,20 @@ func AddViewTemplates() *Repocontroller {
 // GET: /
 func (rc *Repocontroller) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	rc.HomeView.Render(w, nil)
+}
+
+// PostUserHandler is responsible for the home view
+//
+// POST: /
+func (rc *Repocontroller) PostUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("username")
+	http.Redirect(w, r, "/user/ "+username, http.StatusSeeOther)
+}
+
+// GetUserHandler is responsible for the home view
+//
+// GET: /user/:username
+func (rc *Repocontroller) GetUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := mux.Vars(r)["username"]
+	fmt.Fprint(w, username)
 }
--- router/router.go
+++ router/router.go
@@ -13,8 +13,8 @@ func Routes() {
 	r := mux.NewRouter()
 	repoController := controllers.AddViewTemplates()
 	r.HandleFunc("/", repoController.HomeHandler).Methods("GET")
-	r.HandleFunc("/user", nil).Methods("POST")
-	r.HandleFunc("/user/{username}", nil).Methods("GET")
+	r.HandleFunc("/", repoController.PostUserHandler).Methods("POST")
+	r.HandleFunc("/user/{username}", repoController.GetUserHandler).Methods("GET")
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("public"))))
 	http.ListenAndServe(":3000", r)
 }
