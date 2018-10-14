3929d3388e40ca58c6e2166cc625f9869811b1d1
lexville <alekikangethe@gmail.com>
2018-10-15 02:19:48 +0300

    Add the user repos

--- controllers/repocontroller.go
+++ controllers/repocontroller.go
@@ -94,11 +94,108 @@ type UserData struct {
 	UpdatedAt         time.Time   `json:"updated_at"`
 }
 
+// RepoData contains the fields associated with
+// the user repository
+type RepoData struct {
+	ID       int    `json:"id"`
+	NodeID   string `json:"node_id"`
+	Name     string `json:"name"`
+	FullName string `json:"full_name"`
+	Private  bool   `json:"private"`
+	Owner    struct {
+		Login             string `json:"login"`
+		ID                int    `json:"id"`
+		NodeID            string `json:"node_id"`
+		AvatarURL         string `json:"avatar_url"`
+		GravatarID        string `json:"gravatar_id"`
+		URL               string `json:"url"`
+		HTMLURL           string `json:"html_url"`
+		FollowersURL      string `json:"followers_url"`
+		FollowingURL      string `json:"following_url"`
+		GistsURL          string `json:"gists_url"`
+		StarredURL        string `json:"starred_url"`
+		SubscriptionsURL  string `json:"subscriptions_url"`
+		OrganizationsURL  string `json:"organizations_url"`
+		ReposURL          string `json:"repos_url"`
+		EventsURL         string `json:"events_url"`
+		ReceivedEventsURL string `json:"received_events_url"`
+		Type              string `json:"type"`
+		SiteAdmin         bool   `json:"site_admin"`
+	} `json:"owner"`
+	HTMLURL          string      `json:"html_url"`
+	Description      interface{} `json:"description"`
+	Fork             bool        `json:"fork"`
+	URL              string      `json:"url"`
+	ForksURL         string      `json:"forks_url"`
+	KeysURL          string      `json:"keys_url"`
+	CollaboratorsURL string      `json:"collaborators_url"`
+	TeamsURL         string      `json:"teams_url"`
+	HooksURL         string      `json:"hooks_url"`
+	IssueEventsURL   string      `json:"issue_events_url"`
+	EventsURL        string      `json:"events_url"`
+	AssigneesURL     string      `json:"assignees_url"`
+	BranchesURL      string      `json:"branches_url"`
+	TagsURL          string      `json:"tags_url"`
+	BlobsURL         string      `json:"blobs_url"`
+	GitTagsURL       string      `json:"git_tags_url"`
+	GitRefsURL       string      `json:"git_refs_url"`
+	TreesURL         string      `json:"trees_url"`
+	StatusesURL      string      `json:"statuses_url"`
+	LanguagesURL     string      `json:"languages_url"`
+	StargazersURL    string      `json:"stargazers_url"`
+	ContributorsURL  string      `json:"contributors_url"`
+	SubscribersURL   string      `json:"subscribers_url"`
+	SubscriptionURL  string      `json:"subscription_url"`
+	CommitsURL       string      `json:"commits_url"`
+	GitCommitsURL    string      `json:"git_commits_url"`
+	CommentsURL      string      `json:"comments_url"`
+	IssueCommentURL  string      `json:"issue_comment_url"`
+	ContentsURL      string      `json:"contents_url"`
+	CompareURL       string      `json:"compare_url"`
+	MergesURL        string      `json:"merges_url"`
+	ArchiveURL       string      `json:"archive_url"`
+	DownloadsURL     string      `json:"downloads_url"`
+	IssuesURL        string      `json:"issues_url"`
+	PullsURL         string      `json:"pulls_url"`
+	MilestonesURL    string      `json:"milestones_url"`
+	NotificationsURL string      `json:"notifications_url"`
+	LabelsURL        string      `json:"labels_url"`
+	ReleasesURL      string      `json:"releases_url"`
+	DeploymentsURL   string      `json:"deployments_url"`
+	CreatedAt        time.Time   `json:"created_at"`
+	UpdatedAt        time.Time   `json:"updated_at"`
+	PushedAt         time.Time   `json:"pushed_at"`
+	GitURL           string      `json:"git_url"`
+	SSHURL           string      `json:"ssh_url"`
+	CloneURL         string      `json:"clone_url"`
+	SvnURL           string      `json:"svn_url"`
+	Homepage         interface{} `json:"homepage"`
+	Size             int         `json:"size"`
+	StargazersCount  int         `json:"stargazers_count"`
+	WatchersCount    int         `json:"watchers_count"`
+	Language         string      `json:"language"`
+	HasIssues        bool        `json:"has_issues"`
+	HasProjects      bool        `json:"has_projects"`
+	HasDownloads     bool        `json:"has_downloads"`
+	HasWiki          bool        `json:"has_wiki"`
+	HasPages         bool        `json:"has_pages"`
+	ForksCount       int         `json:"forks_count"`
+	MirrorURL        interface{} `json:"mirror_url"`
+	Archived         bool        `json:"archived"`
+	OpenIssuesCount  int         `json:"open_issues_count"`
+	License          interface{} `json:"license"`
+	Forks            int         `json:"forks"`
+	OpenIssues       int         `json:"open_issues"`
+	Watchers         int         `json:"watchers"`
+	DefaultBranch    string      `json:"default_branch"`
+}
+
 // User contains the user data if the user ezists and
 // the error type and message if the user doesn't exist
 type User struct {
-	UserData
-	UserNotFound
+	UserData     UserData
+	UserNotFound UserNotFound
+	RepoData     []RepoData
 }
 
 // GetUserHandler is responsible for getting the user data
@@ -107,7 +204,7 @@ type User struct {
 func (rc *Repocontroller) GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	username := mux.Vars(r)["username"]
 	getUserURL := "https://api.github.com/users/" + username
-	response := apiCall(getUserURL, username)
+	response := apiCall(getUserURL)
 	defer response.Body.Close()
 	if response.StatusCode == 404 {
 		userNotFound := UserNotFound{
@@ -115,9 +212,11 @@ func (rc *Repocontroller) GetUserHandler(w http.ResponseWriter, r *http.Request)
 			Message:   "No user in github has that username",
 		}
 		user := UserData{}
+		var repoData []RepoData
 		data := User{
-			user,
-			userNotFound,
+			UserData:     user,
+			UserNotFound: userNotFound,
+			RepoData:     repoData,
 		}
 		rc.RepoView.Render(w, data)
 		return
@@ -131,16 +230,30 @@ func (rc *Repocontroller) GetUserHandler(w http.ResponseWriter, r *http.Request)
 	if err != nil {
 		log.Fatal("Decoding error: ", err)
 	}
+	getRepoURL := userData.ReposURL
+	response = apiCall(getRepoURL)
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal("Error while reading the body: ", err)
+	}
+	var repoData []RepoData
+	err = json.Unmarshal(body, &repoData)
+	if err != nil {
+		log.Fatal("Decoding error: ", err)
+	}
 	userNotFound := UserNotFound{}
 	data := User{
-		userData,
-		userNotFound,
+		UserData:     userData,
+		UserNotFound: userNotFound,
+		RepoData:     repoData,
 	}
 	rc.RepoView.Render(w, data)
-	// fmt.Fprintln(w, data)
 }
 
-func apiCall(theURL string, username string) *http.Response {
+// apiCall is responsible for making all the api
+// calls needed to get the user data as well
+// as all the user repos
+func apiCall(theURL string) *http.Response {
 	response, err := http.Get(theURL)
 	if err != nil {
 		log.Fatal("Unable to make the request: ", err)
