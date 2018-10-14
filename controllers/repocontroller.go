606223a35451928748550bd5cf4e1f188739e59a
lexville <alekikangethe@gmail.com>
2018-10-15 01:18:10 +0300

    Add user profile info

--- controllers/repocontroller.go
+++ controllers/repocontroller.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"GitMe/view"
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -28,6 +30,7 @@ func AddViewTemplates() *Repocontroller {
 			"base",
 			"templates/repo/user-repo.gohtml",
 			"templates/repo/not-found-user.gohtml",
+			"templates/repo/repo-user-data.gohtml",
 		),
 	}
 }
@@ -55,6 +58,49 @@ type UserNotFound struct {
 	Message   string
 }
 
+// UserData contains all the data associated with the
+// github user
+type UserData struct {
+	Login             string      `json:"login"`
+	ID                int         `json:"id"`
+	NodeID            string      `json:"node_id"`
+	AvatarURL         string      `json:"avatar_url"`
+	GravatarID        string      `json:"gravatar_id"`
+	URL               string      `json:"url"`
+	HTMLURL           string      `json:"html_url"`
+	FollowersURL      string      `json:"followers_url"`
+	FollowingURL      string      `json:"following_url"`
+	GistsURL          string      `json:"gists_url"`
+	StarredURL        string      `json:"starred_url"`
+	SubscriptionsURL  string      `json:"subscriptions_url"`
+	OrganizationsURL  string      `json:"organizations_url"`
+	ReposURL          string      `json:"repos_url"`
+	EventsURL         string      `json:"events_url"`
+	ReceivedEventsURL string      `json:"received_events_url"`
+	Type              string      `json:"type"`
+	SiteAdmin         bool        `json:"site_admin"`
+	Name              string      `json:"name"`
+	Company           string      `json:"company"`
+	Blog              string      `json:"blog"`
+	Location          interface{} `json:"location"`
+	Email             interface{} `json:"email"`
+	Hireable          interface{} `json:"hireable"`
+	Bio               interface{} `json:"bio"`
+	PublicRepos       int         `json:"public_repos"`
+	PublicGists       int         `json:"public_gists"`
+	Followers         int         `json:"followers"`
+	Following         int         `json:"following"`
+	CreatedAt         time.Time   `json:"created_at"`
+	UpdatedAt         time.Time   `json:"updated_at"`
+}
+
+// User contains the user data if the user ezists and
+// the error type and message if the user doesn't exist
+type User struct {
+	UserData
+	UserNotFound
+}
+
 // GetUserHandler is responsible for getting the user data
 //
 // GET: /user/:username
@@ -64,14 +110,34 @@ func (rc *Repocontroller) GetUserHandler(w http.ResponseWriter, r *http.Request)
 	response := apiCall(getUserURL, username)
 	defer response.Body.Close()
 	if response.StatusCode == 404 {
-		UserNotFound := UserNotFound{
+		userNotFound := UserNotFound{
 			ErrorType: "failure",
 			Message:   "No user in github has that username",
 		}
-		rc.RepoView.Render(w, UserNotFound)
+		user := UserData{}
+		data := User{
+			user,
+			userNotFound,
+		}
+		rc.RepoView.Render(w, data)
 		return
 	}
-	fmt.Fprint(w, username)
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal("Error while reading the body: ", err)
+	}
+	userData := UserData{}
+	err = json.Unmarshal(body, &userData)
+	if err != nil {
+		log.Fatal("Decoding error: ", err)
+	}
+	userNotFound := UserNotFound{}
+	data := User{
+		userData,
+		userNotFound,
+	}
+	rc.RepoView.Render(w, data)
+	// fmt.Fprintln(w, data)
 }
 
 func apiCall(theURL string, username string) *http.Response {
